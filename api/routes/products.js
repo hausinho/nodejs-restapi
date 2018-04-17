e3938d996973c4358ff466f57227b78e41d6dd9f
Steffan Haunstrup Thomsen <hausinho@hotmail.com>
2018-04-17 11:12:09 +0200

    changed requests

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,37 +5,67 @@ const mongoose = require("mongoose");
 
 const Product = require("../models/product");
 
-router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Handling GET requests to /products'
-  });
+router.get("/", (req, res, next) => {
+  Product.find()
+    .select("name price _id")
+    .exec()
+    .then(docs => {
+      const response = {
+        count: docs.length,
+        products: docs.map(doc => {
+          return {
+            name: doc.name,
+            price: doc.price,
+            _id: doc._id
+          };
+        })
+      };
+  
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
-router.post('/', (req, res, next) => {
-
+router.post("/", (req, res, next) => {
   const product = new Product({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
     price: req.body.price
   });
-
-  product.save().then(result => {
-    console.log('result: ', result)
-  });
-  res.status(200).json({
-    message: 'Handling POST requests to /products',
-    createdProduct: product
-  });
+  product
+    .save()
+    .then(result => {
+      res.status(201).json({
+        message: "Created product successfully",
+        createdProduct: {
+            name: result.name,
+            price: result.price,
+            _id: result._id
+        }
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
-router.get('/:productId', (req, res, next) => {
+router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
   Product.findById(id)
+    .select('name price _id')
     .exec()
     .then(doc => {
-      console.log("From database", doc);
       if (doc) {
-        res.status(200).json(doc);
+        res.status(200).json({
+            product: doc
+        });
       } else {
         res
           .status(404)
@@ -43,21 +73,43 @@ router.get('/:productId', (req, res, next) => {
       }
     })
     .catch(err => {
-      console.log(err);
       res.status(500).json({ error: err });
     });
 });
 
 router.patch('/:productId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Updated'
-  });
+  const id = req.params.productId;
+  const updateOpt = {};
+  for (const opt of req.body) {
+    updateOpt[opt.propName] = opt.value
+  }
+  Product.update({_id: id}, {$set: updateOpt })
+  .exec()
+  .then(result => {
+    res.status(200).json({
+        message: 'Product updated'
+    });
+  })
+  .catch(err => {
+    res.status(500).json({
+      error: err
+    });
+  });  
 });
 
 router.delete('/:productId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Deleted'
-  });
+  const id = req.params.productId;
+  Product.remove({_id: id})
+  .exec()
+  .then(result => {
+    res.status(200).json(result)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error: err
+    })
+  })
 });
 
 module.exports = router;
